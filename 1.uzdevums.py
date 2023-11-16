60bc2ea635c64f595c09193846c0bb9870d7da1f
unknown <emilija.cipkina@grobinasskola.lv>
2023-11-16 09:37:03 +0200

    Init commit

--- 1.uzdevums.py
+++ 1.uzdevums.py
@@ -2,4 +2,9 @@
 Izveidojiet Python programmu, kas saskaita no 1 līdz lietotāja ievadītam skaitlim, izmantojot for ciklu.
 Izmantojiet GIT, lai izsekotu izmaiņas programmas kodā un izveidotu komitus, tos noevoetojot arī GITHUB.
 """
-
+skaitlis=int(input("Ievadiet skaitli: "))
+if skaitlis < 1:
+        print("Ievadiet pozitīvu skaitli.")
+else:
+    for skaitlis1 in range(1,skaitlis+1):
+           print (skaitlis1)
