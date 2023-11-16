42ab94d0fdb3b998cbccddbd3c0b2df8fbbd0233
unknown <emilija.cipkina@grobinasskola.lv>
2023-11-16 09:22:00 +0200

    first commit

---
+++ 1.uzdevums.py
@@ -0,0 +1,5 @@
+"""
+Izveidojiet Python programmu, kas saskaita no 1 līdz lietotāja ievadītam skaitlim, izmantojot for ciklu.
+Izmantojiet GIT, lai izsekotu izmaiņas programmas kodā un izveidotu komitus, tos noevoetojot arī GITHUB.
+"""
+
